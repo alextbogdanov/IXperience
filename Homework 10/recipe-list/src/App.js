3eca05ee9f049240dc503777cf8fa73f084e5199
Alexander Bogdanov <alex.bogdanovv14@gmail.com>
2022-07-28 03:01:50 +0200

    Implementing variable routing and authentication with firebase

--- Homework 10/recipe-list/src/App.js
+++ Homework 10/recipe-list/src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase/firebase';
+
 import RecipesPage from './pages/RecipesPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import Navbar from './components/Navbar';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, [])
+
   return (
     <BrowserRouter>
-      <Navbar />
+      <Navbar user={user} />
       <Routes>
         <Route path="/" element={<RecipesPage />} />
         <Route path="/login" element={<LoginPage />} />
