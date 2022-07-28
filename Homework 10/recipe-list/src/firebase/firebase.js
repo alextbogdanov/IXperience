5061ee7e4a51c592881aceb20bd5406ed71e345e
Alexander Bogdanov <alex.bogdanovv14@gmail.com>
2022-07-28 02:13:17 +0200

    Added authentication to recipe list

--- Homework 10/recipe-list/src/firebase/firebase.js
+++ Homework 10/recipe-list/src/firebase/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC5IBiJHhtMEzePHdR3FzV3gjQ2fT-J40g",
@@ -14,7 +15,9 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
 export {
-  db
+  db,
+  auth
 };
--- Homework 10/recipe-list/src/pages/LoginPage.js
+++ Homework 10/recipe-list/src/pages/LoginPage.js
@@ -1,27 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebase/firebase';
 
 export default function LoginPage() {
+  const [emailInput, setEmailInput] = useState('');
+  const [passwordInput, setPasswordInput] = useState('');
+
+  const navigate = useNavigate();
+
+  async function onFormSubmit(e) {
+    e.preventDefault();
+
+    try {
+      await signInWithEmailAndPassword(auth, emailInput, passwordInput);
+      navigate('/');
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
   return (
     <div className='container mt-5'>
       <div className='card p-5'>
         <h1>Login</h1>
         <hr className="mb-5" />
-        <form>
+        <form onSubmit={onFormSubmit}>
           <div className="mb-3">
             <label className="form-label">Email address</label>
             <input
               type="email"
-              className="form-control" />
+              className="form-control"
+              value={emailInput}
+              onChange={(e) => {setEmailInput(e.target.value)}}
+              />
           </div>
           <div className="mb-3">
             <label className="form-label">Password</label>
             <input
               type="password"
               className="form-control"
+              value={passwordInput}
+              onChange={(e) => {setPasswordInput(e.target.value)}}
             />
           </div>
           <div className='d-grid'>
-            <button className='btn btn-secondary'>Login</button>
+            <button className='btn btn-secondary' type="submit">Login</button>
           </div>
         </form>
       </div>
